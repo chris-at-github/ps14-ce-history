860bdf4d42cb8cce0bfd4ca2ffcfd28ce8feca77
Christian Pschorr <c.pschorr@datamints.com>
2022-01-18 14:04:16 +0100

    Initialer Modulaufbau

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,49 @@
+<?php
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Icon Text Module von TT-Content
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+	array(
+		'LLL:EXT:ce_history/Resources/Private/Language/locallang_tca.xlf:tx_ce_history.title',
+		'ce_history',
+		'ps14-content-history'
+	),
+	'CType',
+	'ce_history'
+);
+
+$GLOBALS['TCA']['tt_content']['types']['ce_history'] = [
+	'showitem' => '
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.general;general,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.header;xoHeader, tx_xo_elements,
+		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.appearance,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.frames;frames,
+		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
+			--palette--;;hidden,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.visibility;visibility,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.access;access,
+		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.extended
+	',
+];
+
+$GLOBALS['TCA']['tt_content']['types']['ce_history']['columnsOverrides']['tx_xo_elements']['config']['overrideChildTca'] = [
+	'columns' => [
+		'record_type' => [
+			'config' => [
+				'items' => [
+					['LLL:EXT:ce_history/Resources/Private/Language/locallang_tca.xlf:tx_xo_domain_model_elements.record_type.default', 'ce_history_default'],
+				],
+				'default' => 'ce_history_default'
+			]
+		]
+	],
+	'types' => [
+		'ce_history_default' => [
+			'showitem' => '
+				l10n_diffsource, record_type, --palette--;;header, description,
+				--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
+				--palette--;;visibility,
+				--palette--;;access',
+		],
+	]
+];
