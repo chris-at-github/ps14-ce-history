1c100b25942cdb6180de9f2a0a1607e06c1b711a
Christian Pschorr <c.pschorr@datamints.com>
2022-01-21 23:44:54 +0100

    Zweiter Prototype nun mit window.scroll

--- Resources/Public/Js/history.js
+++ Resources/Public/Js/history.js
@@ -2,71 +2,156 @@
 	'use strict';
 
 	xna.on('documentLoaded', function() {
-		let offset = 100;
-		let progressHeight = 0;
-		let indicatorHeight = 0;
-		let node = document.querySelector('.ce-history');
-		let indicator = node.querySelector('.indicator');
-		let container = node.querySelector('ul');
-		let watch = node.querySelector('.watch');
-		let maxHeight = container.offsetHeight;
-		let options = {
-			threshold: []
-		};
 
-		for(let i = 0; i <= 1.0; i += 0.1) {
-			options.threshold.push(i);
-		}
-		// for (let i=0; i<=1.0; i+= 0.01) {
-		// 	options.threshold.push(i);
-		// }
+		let node = document.querySelector('.ce-history');
+		let container = node.querySelector('ul');
+
+		let lastKnownScrollPosition = 0;
+		let ticking = false;
+
+		let containerTop = (window.scrollY + container.getBoundingClientRect().top) - window.outerHeight;
+		let indicator = node.querySelector('.indicator');
+		let maxHeight = container.getBoundingClientRect().height;
 
 		let steps = container.querySelectorAll('li');
-		// steps.forEach(function(step, index) {
-		//
-		// });
-
-
 
 		// maximal bis zum letzten Element scrollen
 		let lastChildHeight = steps[steps.length - 1].offsetHeight;
 		maxHeight = maxHeight - lastChildHeight;
 
-		// if(lastChildHeight < offset) {
-		// 	maxHeight = maxHeight - steps[steps.length - 1].offsetHeight + 100;
-		// 	console.log(maxHeight);
+		//console.log();
+		// console.log(window.outerHeight);
+
+		// let spy = function() {
 		//
-		// } else {
-		// 	maxHeight = maxHeight - steps[steps.length - 1].offsetHeight;
-		// }
+		// };
 
-		let os = new IntersectionObserver(function(entries) {
-			if(typeof(entries[0]) !== 'undefined') {
-				let scrollHeight = (Math.floor(entries[0].intersectionRect.height));
+		let scrollOnContainer = function() {
+			let containerScrollPosition = lastKnownScrollPosition - containerTop;
 
-				//console.log(container.getBoundingClientRect());
-				console.log(maxHeight, scrollHeight, progressHeight);
-				// console.log(entries[0]);
-
-				if(scrollHeight >= progressHeight) {
-					progressHeight = scrollHeight;
-					indicatorHeight = (scrollHeight - offset);
-
-					console.log(indicatorHeight);
-
-					if(indicatorHeight > maxHeight) {
-						indicatorHeight = maxHeight;
-					}
-
-					if(indicatorHeight > 0 && indicatorHeight <= maxHeight) {
-						indicator.style.height = indicatorHeight  + 'px';
-					}
-
-				// 	console.log(percent);
-				}
+			if(containerScrollPosition > maxHeight) {
+				containerScrollPosition = maxHeight;
 			}
-		}, options);
 
-		os.observe(watch);
+			if(containerScrollPosition <= maxHeight) {
+				indicator.style.height = Math.floor(containerScrollPosition)  + 'px';
+			}
+
+
+		}
+
+
+		window.addEventListener('scroll', function() {
+			lastKnownScrollPosition = window.scrollY;
+
+			if(ticking === false) {
+				window.requestAnimationFrame(function() {
+					if(lastKnownScrollPosition >= containerTop) {
+						scrollOnContainer();
+					}
+					ticking = false;
+				});
+
+				ticking = true;
+			}
+		});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// let offset = 100;
+		// let progressHeight = 0;
+		// let indicatorHeight = 0;
+		// let node = document.querySelector('.ce-history');
+		// let indicator = node.querySelector('.indicator');
+		// let container = node.querySelector('ul');
+		// let watch = node.querySelector('.watch');
+		// let maxHeight = container.offsetHeight;
+		// let options = {
+		// 	threshold: []
+		// };
+		//
+		// for(let i = 0; i <= 1.0; i += 0.1) {
+		// 	options.threshold.push(i);
+		// }
+		// // for (let i=0; i<=1.0; i+= 0.01) {
+		// // 	options.threshold.push(i);
+		// // }
+		//
+		// let steps = container.querySelectorAll('li');
+		// // steps.forEach(function(step, index) {
+		// //
+		// // });
+		//
+		//
+		//
+		// // maximal bis zum letzten Element scrollen
+		// let lastChildHeight = steps[steps.length - 1].offsetHeight;
+		// maxHeight = maxHeight - lastChildHeight;
+		//
+		//
+		// let containerObserver = new IntersectionObserver(function(entries) {
+		// 	if(typeof(entries[0]) !== 'undefined') {
+		// 		let scrollHeight = (Math.floor(entries[0].intersectionRect.height));
+		//
+		// 		if(scrollHeight >= progressHeight) {
+		// 			progressHeight = scrollHeight;
+		// 			indicatorHeight = (scrollHeight - offset);
+		//
+		// 			if(indicatorHeight > maxHeight) {
+		// 				indicatorHeight = maxHeight;
+		// 			}
+		//
+		// 			if(indicatorHeight > 0 && indicatorHeight <= maxHeight) {
+		// 				indicator.style.height = indicatorHeight  + 'px';
+		// 				steps[0].classList.add('active');
+		// 			}
+		// 		}
+		// 	}
+		// }, options);
+		//
+		// containerObserver.observe(watch);
+		//
+		//
+		// let stepObserver = new IntersectionObserver(function(entries, observer) {
+		// 	if(typeof(entries[0]) !== 'undefined' && entries[0].isIntersecting === true) {
+		//
+		// 		if((entries[0].intersectionRect.height - 165) >= 0) {
+		// 			console.log((entries[0].intersectionRect.height - offset));
+		// 			entries[0].target.classList.add('active');
+		// 		}
+		// 	}
+		// }, {
+		// 	threshold: [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+		// });
+		//
+		// steps.forEach(function(step, index) {
+		// 	stepObserver.observe(step);
+		// });
 	});
 })();
